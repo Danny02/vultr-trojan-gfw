c10c82b63bf39ee15ae837879124c85b7c8bfd27
Daniel Heinrich <dannynullzwo@gmail.com>
2024-03-14 12:41:50 +0100

    document variables

--- modules/trojan/variables.tf
+++ modules/trojan/variables.tf
@@ -1,19 +1,41 @@
 variable "DOMAIN" {
   type = string
+  description = "The sub domain Vultr DNS is configured as ns record."
+  validation {
+    condition = can(regex(".+[.].+[.].+", var.DOMAIN))
+    error_message = "module only works with a sub domain currently"
+  }
 }
 
 variable "DOMAIN_ID" {
   type = string
+  description = "id of vultr_dns_domain"
+  validation {
+    condition = can(regex("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", var.DOMAIN_ID))
+    error_message = "no valid uuid"
+  }
 }
 
 variable "SSH_KEY_ID" {
   type = string
+  description = "id of vultr_ssh_key"
+  validation {
+    condition = can(regex("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", var.SSH_KEY_ID))
+    error_message = "no valid uuid"
+  }
 }
 
 variable "REGION" {
   type = string
+  description = "A Vultr region to a deploy the trojan instances to"
+  validation {
+    condition = can(regex("^[a-z]{3}$", var.REGION))
+    error_message = "See https://api.vultr.com/v2/regions for a list of valid regions"
+  }
 }
 
 variable "PASSWORD" {
   type = string
+  description = "Password to use for trojan connection"
+  sensitive = true
 }
--- variables.tf
+++ variables.tf
@@ -1,13 +1,67 @@
 variable "DOMAIN" {
-  type = string
+  type        = string
+  description = "The sub domain Vultr DNS is configured as ns record."
+  validation {
+    condition = can(regex(".+[.].+[.].+", var.DOMAIN))
+    error_message = "module only works with a sub domain currently"
+  }
 }
 
-variable "REGIONS" {
-  type = list(string)
+variable "VULTR_API_KEY" {
+  type = string
+  description = "Vultr API key to create instances with"
 }
 
 variable "SSH_KEY" {
   type = string
+  description = "A ssh public key to access the trojan instances"
+  validation {
+    condition = can(regex("^ssh-[0-9a-z]+ AAAA[0-9A-Za-z+]+[=]{0,3} .+$", var.SSH_KEY))
+    error_message = "must be a single line ssh public key, e.g. content from ~/.ssh/id_rsa.pub"
+  }
 }
 
-variable "VULTR_API_KEY" {}
+variable "REGIONS" {
+  type = list(string)
+  description = "A list of Vultr regions to a deploy trojan instances to"
+  default = ["lax"]
+  validation {
+    condition = can([for r in var.REGIONS : regex("^[a-z]{3}$", r)])
+    error_message = "See https://api.vultr.com/v2/regions for a list of valid regions"
+  }
+}
+
+# currently available regions
+#
+# ams Amsterdam
+# atl Atlanta
+# blr Bangalore
+# bom Mumbai
+# cdg Paris
+# del Delhi NCR
+# dfw Dallas
+# ewr New Jersey
+# fra Frankfurt
+# hnl Honolulu
+# icn Seoul
+# itm Osaka
+# jnb Johannesburg
+# lax Los Angeles
+# lhr London
+# mad Madrid
+# man Manchester
+# mel Melbourne
+# mex Mexico City
+# mia Miami
+# nrt Tokyo
+# ord Chicago
+# sao São Paulo
+# scl Santiago
+# sea Seattle
+# sgp Singapore
+# sjc Silicon Valley
+# sto Stockholm
+# syd Sydney
+# tlv Tel Aviv
+# waw Warsaw
+# yto Toronto
